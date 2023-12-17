e3e05c45e4c148e5e19bc29878447e7be462acf0
LIU Che <che.liu@geometryrobot.com>
2023-12-18 00:18:40 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,5 @@
+#include "supermorpion.h"
+
+int main(){
+
+}
---
+++ supermorpion.h
@@ -0,0 +1,16 @@
+#ifndef SUPER_MORPION_H
+    #define SUPER_MORPION_H
+
+typedef struct supermorpion
+{
+    /* data */
+}morpion;
+
+typedef struct supermorpion_noeud
+{
+    morpion M;
+    morpion * nextM[];
+}noeud;
+
+
+#endif
